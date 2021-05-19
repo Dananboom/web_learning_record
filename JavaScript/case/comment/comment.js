ee82526b542fbfb84a6d9499a3bd063cf7f4db79
danan <2907355904@qq.com>
2021-05-19 21:01:49 +0800

    #

--- JavaScript/case/comment/comment.js
+++
@@ -1,7 +0,0 @@
-var myButton = document.querySelector("button")
-var text = document.querySelector(".two")
-myButton.onclick = function() {
-    var comment = document.querySelector(".one")
-    text.innerHTML += '<div>' + comment.value + '</div>'
-
-}
