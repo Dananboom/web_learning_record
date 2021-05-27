bc2eb3d1ebe8e1ca4fe9153bbe202a4f75f218e4
danan <2907355904@qq.com>
2021-05-27 14:55:19 +0800

    #

--- JavaScript/MDN/构建函js的砖块/gallery-start/main.js
+++ JavaScript/MDN/构建函js的砖块/gallery-start/main.js
@@ -1,13 +1,32 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
-/* 添加图片循环 */
+/* Looping through images */
+for(let i = 1; i <= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', function(e) {
+    let src = e.target.getAttribute('src');
+    displayedImage.setAttribute('src', src);
+  });
+}
 
-/* 编写 变暗/变量 按钮功能 */
+/* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function() {
+  if (btn.getAttribute('class') === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+})
+
+
